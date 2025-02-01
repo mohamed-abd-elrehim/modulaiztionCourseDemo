51a74f4bb644950ba465d5f5743c3b9411ab0666
mohamedabdelrehim <mohamed.abdelrehim.abdelhady@gmail.com>
2025-02-02 00:53:02 +0200

    feat(datasource): setup Ktor client

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -1,7 +1,16 @@
+
 apply {
     from("$rootDir/library-build.gradle")
 }
 
 
 dependencies {
+    "implementation"(project(":hero:hero-domain"))
+    "implementation"(libs.ktor.android)
+    "implementation"(libs.ktor.core)
+    "implementation"(libs.ktor.serialization)
+    "testImplementation"(libs.ktor.mock) // `mock` is usually used for testing
+
+
+
 }
